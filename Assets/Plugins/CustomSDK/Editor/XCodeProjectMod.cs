b1b76e6b9f85617b513e9e87483b014cb3e3c2b0
sy545633799 <545633799@qq.com>
2018-07-27 11:39:27 +0800

    xcode test

--- Assets/Plugins/CustomSDK/Editor/XCodeProjectMod.cs
+++ Assets/Plugins/CustomSDK/Editor/XCodeProjectMod.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.iOS.Xcode;
+//using UnityEditor.iOS.Xcode;
 using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
@@ -13,12 +13,9 @@ public class XCodeProjectMod
     {
         if (Directory.Exists(dstPath))
         {
-            Directory.Delete(dstPath);
+            Directory.Delete(dstPath, true);
         }
-        if (Directory.Exists(srcPath))
-        {
-            Directory.Delete(srcPath);
-        }
+
         Directory.CreateDirectory(dstPath);
 
         foreach (var file in Directory.GetFiles(srcPath))
@@ -41,132 +38,139 @@ public class XCodeProjectMod
             return;
         }
 
-        #region 修改工程
-//        string projPath = PBXProject.GetPBXProjectPath(path);
-//        //Debug.Log ("projPath: " + projPath);
-//        PBXProject proj = new PBXProject();
-//        string fileText = File.ReadAllText(projPath);
-//        proj.ReadFromString(fileText);
-//        //Debug.Log ("fileText: " + fileText);
-
-//        string targetName = PBXProject.GetUnityTargetName();//Unity-iPhone
-//        string targetGuid = proj.TargetGuidByName(targetName);
-//        //Debug.Log ("targetName: " + targetName);
-//        //Debug.Log ("targetGuid: " + targetGuid);
-
-//        // common
-//        proj.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
-//        //proj.SetBuildProperty(targetGuid, "DEBUG_INFORMATION_FORMAT", "dwarf");
-//        proj.AddBuildProperty(targetGuid, "OTHER_LDFLAGS", "-ObjC");
-//        //proj.AddBuildProperty (targetGuid, "OTHER_LDFLAGS", "-all_load ");
-//        //proj.SetBuildProperty(targetGuid, "FRAMEWORK_SEARCH_PATHS", "$(inherited)");
-//        //proj.AddBuildProperty (targetGuid, "FRAMEWORK_SEARCH_PATHS","xxxxxxx");
-//        //proj.AddBuildProperty (targetGuid, "HEADER_SEARCH_PATHS","xxxxxxx");
-//        //proj.AddBuildProperty (targetGuid, "LIBRARY_SEARCH_PATHS","xxxxxxx");
-
-//        // umeng
-//        proj.AddFrameworkToProject(targetGuid, "libz.tbd", false);
-//        // bugly
-//        proj.AddFrameworkToProject(targetGuid, "libz.tbd", false);
-//        proj.AddFrameworkToProject(targetGuid, "libc++.tbd", false);
-//        proj.AddFrameworkToProject(targetGuid, "Security.framework", false);
-//        proj.AddFrameworkToProject(targetGuid, "SystemConfiguration.framework", false);
-//        proj.AddFrameworkToProject(targetGuid, "JavaScriptCore.framework", true);
-//        // wechat
-//        proj.AddFrameworkToProject(targetGuid, "libz.tbd", false);
-//        proj.AddFrameworkToProject(targetGuid, "libsqlite3.0.tbd", false);
-//        proj.AddFrameworkToProject(targetGuid, "libc++.tbd", false);
-//        proj.AddFrameworkToProject(targetGuid, "SystemConfiguration.framework", false);
-//        proj.AddFrameworkToProject(targetGuid, "Security.framework", false);
-//        proj.AddFrameworkToProject(targetGuid, "CoreTelephony.framework", false);
-//        proj.AddFrameworkToProject(targetGuid, "CFNetwork.framework", false);
-//        // ios定位
-//        proj.AddFrameworkToProject(targetGuid, "CoreLocation.framework", false);
-
-//        // 为魔窗添加Associated Domains域名
-//        string fileName = "my.entitlements";    // 这个名字任意
-//        string filePath = Path.Combine(path, fileName);
-//        //Debug.Log ("filePath: " + filePath);
-//        string fileContent = @"<?xml version=""1.0"" encoding=""UTF-8""?>
-//<!DOCTYPE plist PUBLIC ""-//Apple//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd 
-
-//"">
-//<plist version=""1.0"">
-//<dict>
-//    <key>com.apple.developer.associated-domains</key>
-//    <array>
-//        <string>applinks:sjsbna.mlinks.cc 
-
-//</string>
-//    </array>
-//</dict>
-//</plist>";
-//        File.WriteAllText(filePath, fileContent);
-//        proj.AddFile(filePath, fileName);
-//        proj.SetBuildProperty(targetGuid, "CODE_SIGN_ENTITLEMENTS", fileName);
-//        // save changed
-//        File.WriteAllText(projPath, proj.WriteToString());
-
-        #endregion
+        string fromPath = Path.Combine(Directory.GetParent(Application.dataPath).FullName, "ios_icon");
+        string targetPath = Path.Combine(path, "Unity-iPhone/Images.xcassets/AppIcon.appiconset");
+        CopyAndReplaceDirectory(fromPath, targetPath);
 
 
-        #region 修改plist
-        string plistPath = Path.Combine(path, "Info.plist");
-        Debug.Log("plistPath: " + plistPath);
-        PlistDocument plist = new PlistDocument();
-        string plistFileText = File.ReadAllText(plistPath);
-        plist.ReadFromString(plistFileText);
-        PlistElementDict rootDict = plist.root;
+        //#region 修改工程
+        ////        string projPath = PBXProject.GetPBXProjectPath(path);
+        ////        //Debug.Log ("projPath: " + projPath);
+        ////        PBXProject proj = new PBXProject();
+        ////        string fileText = File.ReadAllText(projPath);
+        ////        proj.ReadFromString(fileText);
+        ////        //Debug.Log ("fileText: " + fileText);
 
-        // 设置语言语言环境
-        //rootDict.SetString("CFBundleDevelopmentRegion", "zh_CN");
-        //      rootDict.SetString("CFBundleDevelopmentRegion", "en");
+        ////        string targetName = PBXProject.GetUnityTargetName();//Unity-iPhone
+        ////        string targetGuid = proj.TargetGuidByName(targetName);
+        ////        //Debug.Log ("targetName: " + targetName);
+        ////        //Debug.Log ("targetGuid: " + targetGuid);
 
-        // 一些权限声明
-        rootDict.SetString("NSContactsUsageDescription", "App需要您的同意,才能访问通讯录");
-        rootDict.SetString("NSPhotoLibraryUsageDescription", "App需要您的同意,才能访问相册");
-        rootDict.SetString("NSCameraUsageDescription", "App需要您的同意,才能访问相机");
-        rootDict.SetString("NSLocationUsageDescription", "App需要您的同意,才能访问位置");
-        rootDict.SetString("NSLocationAlwaysUsageDescription", "App需要您的同意,才能始终访问位置");
-        rootDict.SetString("NSLocationWhenInUseUsageDescription", "App需要您的同意,才能在使用期间访问位置");
-        rootDict.SetString("NSCalendarsUsageDescription", "App需要您的同意,才能访问日历");
-        rootDict.SetString("NSRemindersUsageDescription", "App需要您的同意,才能访问提醒事项");
-        rootDict.SetString("NSMotionUsageDescription", "App需要您的同意,才能访问运动与健身");
-        rootDict.SetString("NSHealthUpdateUsageDescription", "App需要您的同意,才能访问健康更新");
-        rootDict.SetString("NSHealthShareUsageDescription", "App需要您的同意,才能访问健康分享");
-        rootDict.SetString("NSBluetoothPeripheralUsageDescription", "App需要您的同意,才能访问蓝牙");
-        rootDict.SetString("NSAppleMusicUsageDescription", "App需要您的同意,才能访问媒体资料库");
+        ////        // common
+        ////        proj.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
+        ////        //proj.SetBuildProperty(targetGuid, "DEBUG_INFORMATION_FORMAT", "dwarf");
+        ////        proj.AddBuildProperty(targetGuid, "OTHER_LDFLAGS", "-ObjC");
+        ////        //proj.AddBuildProperty (targetGuid, "OTHER_LDFLAGS", "-all_load ");
+        ////        //proj.SetBuildProperty(targetGuid, "FRAMEWORK_SEARCH_PATHS", "$(inherited)");
+        ////        //proj.AddBuildProperty (targetGuid, "FRAMEWORK_SEARCH_PATHS","xxxxxxx");
+        ////        //proj.AddBuildProperty (targetGuid, "HEADER_SEARCH_PATHS","xxxxxxx");
+        ////        //proj.AddBuildProperty (targetGuid, "LIBRARY_SEARCH_PATHS","xxxxxxx");
 
-        //PlistElementArray urlTypes = rootDict.CreateArray("CFBundleURLTypes");
-        //// add url scheme for wechat
-        //string wechat_appid = "wx8888888888888";
-        //PlistElementDict wxUrl = urlTypes.AddDict();
-        //wxUrl.SetString("CFBundleTypeRole", "Editor");
-        //wxUrl.SetString("CFBundleURLName", "weixin");
-        //wxUrl.SetString("CFBundleURLSchemes", wechat_appid);
-        //PlistElementArray wxUrlScheme = wxUrl.CreateArray("CFBundleURLSchemes");
-        //wxUrlScheme.AddString(wechat_appid);
-        //// 白名单 for wechat
-        //PlistElementArray queriesSchemes = rootDict.CreateArray("LSApplicationQueriesSchemes");
-        //queriesSchemes.AddString("wechat");
-        //queriesSchemes.AddString("weixin");
-        //queriesSchemes.AddString(wechat_appid);
-        //// add url scheme magic window
-        //string mw_sheme = "xxxx"; // xxxx://
-        //PlistElementDict mwUrl = urlTypes.AddDict();
-        //mwUrl.SetString("CFBundleTypeRole", "Editor");
-        //mwUrl.SetString("CFBundleURLName", "magicwindow");
-        ////mwUrl.SetString("CFBundleURLSchemes", mw_sheme );
-        //PlistElementArray mwUrlScheme = mwUrl.CreateArray("CFBundleURLSchemes");
-        //mwUrlScheme.AddString(mw_sheme);
+        ////        // umeng
+        ////        proj.AddFrameworkToProject(targetGuid, "libz.tbd", false);
+        ////        // bugly
+        ////        proj.AddFrameworkToProject(targetGuid, "libz.tbd", false);
+        ////        proj.AddFrameworkToProject(targetGuid, "libc++.tbd", false);
+        ////        proj.AddFrameworkToProject(targetGuid, "Security.framework", false);
+        ////        proj.AddFrameworkToProject(targetGuid, "SystemConfiguration.framework", false);
+        ////        proj.AddFrameworkToProject(targetGuid, "JavaScriptCore.framework", true);
+        ////        // wechat
+        ////        proj.AddFrameworkToProject(targetGuid, "libz.tbd", false);
+        ////        proj.AddFrameworkToProject(targetGuid, "libsqlite3.0.tbd", false);
+        ////        proj.AddFrameworkToProject(targetGuid, "libc++.tbd", false);
+        ////        proj.AddFrameworkToProject(targetGuid, "SystemConfiguration.framework", false);
+        ////        proj.AddFrameworkToProject(targetGuid, "Security.framework", false);
+        ////        proj.AddFrameworkToProject(targetGuid, "CoreTelephony.framework", false);
+        ////        proj.AddFrameworkToProject(targetGuid, "CFNetwork.framework", false);
+        ////        // ios定位
+        ////        proj.AddFrameworkToProject(targetGuid, "CoreLocation.framework", false);
 
-        //// for wechat
-        //PlistElementArray backModes = rootDict.CreateArray("UIBackgroundModes");
-        //backModes.AddString("remote-notification");
+        ////        // 为魔窗添加Associated Domains域名
+        ////        string fileName = "my.entitlements";    // 这个名字任意
+        ////        string filePath = Path.Combine(path, fileName);
+        ////        //Debug.Log ("filePath: " + filePath);
+        ////        string fileContent = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+        ////<!DOCTYPE plist PUBLIC ""-//Apple//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd 
 
-        // 保存修改
-        File.WriteAllText(plistPath, plist.WriteToString());
+        ////"">
+        ////<plist version=""1.0"">
+        ////<dict>
+        ////    <key>com.apple.developer.associated-domains</key>
+        ////    <array>
+        ////        <string>applinks:sjsbna.mlinks.cc 
 
-        #endregion
+        ////</string>
+        ////    </array>
+        ////</dict>
+        ////</plist>";
+        ////        File.WriteAllText(filePath, fileContent);
+        ////        proj.AddFile(filePath, fileName);
+        ////        proj.SetBuildProperty(targetGuid, "CODE_SIGN_ENTITLEMENTS", fileName);
+        ////        // save changed
+        ////        File.WriteAllText(projPath, proj.WriteToString());
+
+        //#endregion
+
+
+        //#region 修改plist
+        //string plistPath = Path.Combine(path, "Info.plist");
+        //Debug.Log("plistPath: " + plistPath);
+        //PlistDocument plist = new PlistDocument();
+        //string plistFileText = File.ReadAllText(plistPath);
+        //plist.ReadFromString(plistFileText);
+        //PlistElementDict rootDict = plist.root;
+
+        //// 设置语言语言环境
+        ////rootDict.SetString("CFBundleDevelopmentRegion", "zh_CN");
+        ////      rootDict.SetString("CFBundleDevelopmentRegion", "en");
+
+        //// 一些权限声明
+        //rootDict.SetString("NSContactsUsageDescription", "App需要您的同意,才能访问通讯录");
+        //rootDict.SetString("NSPhotoLibraryUsageDescription", "App需要您的同意,才能访问相册");
+        //rootDict.SetString("NSPhotoLibraryAddUsageDescription", "App需要您的同意,才能保存照片");
+        //rootDict.SetString("NSCameraUsageDescription", "App需要您的同意,才能访问相机");
+        //rootDict.SetString("NSLocationUsageDescription", "App需要您的同意,才能访问位置");
+        //rootDict.SetString("NSLocationAlwaysUsageDescription", "App需要您的同意,才能始终访问位置");
+        //rootDict.SetString("NSLocationWhenInUseUsageDescription", "App需要您的同意,才能在使用期间访问位置");
+        //rootDict.SetString("NSCalendarsUsageDescription", "App需要您的同意,才能访问日历");
+        //rootDict.SetString("NSRemindersUsageDescription", "App需要您的同意,才能访问提醒事项");
+        //rootDict.SetString("NSMotionUsageDescription", "App需要您的同意,才能访问运动与健身");
+        //rootDict.SetString("NSHealthUpdateUsageDescription", "App需要您的同意,才能访问健康更新");
+        //rootDict.SetString("NSHealthShareUsageDescription", "App需要您的同意,才能访问健康分享");
+        //rootDict.SetString("NSBluetoothPeripheralUsageDescription", "App需要您的同意,才能访问蓝牙");
+        //rootDict.SetString("NSMicrophoneUsageDescription", "App需要您的同意,才能访问麦克风");
+        //rootDict.SetString("NSAppleMusicUsageDescription", "App需要您的同意,才能访问媒体资料库");
+
+        ////PlistElementArray urlTypes = rootDict.CreateArray("CFBundleURLTypes");
+        ////// add url scheme for wechat
+        ////string wechat_appid = "wx8888888888888";
+        ////PlistElementDict wxUrl = urlTypes.AddDict();
+        ////wxUrl.SetString("CFBundleTypeRole", "Editor");
+        ////wxUrl.SetString("CFBundleURLName", "weixin");
+        ////wxUrl.SetString("CFBundleURLSchemes", wechat_appid);
+        ////PlistElementArray wxUrlScheme = wxUrl.CreateArray("CFBundleURLSchemes");
+        ////wxUrlScheme.AddString(wechat_appid);
+        ////// 白名单 for wechat
+        ////PlistElementArray queriesSchemes = rootDict.CreateArray("LSApplicationQueriesSchemes");
+        ////queriesSchemes.AddString("wechat");
+        ////queriesSchemes.AddString("weixin");
+        ////queriesSchemes.AddString(wechat_appid);
+        ////// add url scheme magic window
+        ////string mw_sheme = "xxxx"; // xxxx://
+        ////PlistElementDict mwUrl = urlTypes.AddDict();
+        ////mwUrl.SetString("CFBundleTypeRole", "Editor");
+        ////mwUrl.SetString("CFBundleURLName", "magicwindow");
+        //////mwUrl.SetString("CFBundleURLSchemes", mw_sheme );
+        ////PlistElementArray mwUrlScheme = mwUrl.CreateArray("CFBundleURLSchemes");
+        ////mwUrlScheme.AddString(mw_sheme);
+
+        ////// for wechat
+        ////PlistElementArray backModes = rootDict.CreateArray("UIBackgroundModes");
+        ////backModes.AddString("remote-notification");
+
+        //// 保存修改
+        //File.WriteAllText(plistPath, plist.WriteToString());
+
+        //#endregion
     }
 }
--- Assets/Plugins/CustomSDK/PhotoManger.cs
+++ Assets/Plugins/CustomSDK/PhotoManger.cs
@@ -8,46 +8,157 @@ using UnityEngine.UI;
 
 public class PhotoManger : MonoBehaviour {
 
-    public Text msgText;
-    public void SaveToAlbum()
+    public static PhotoManger Instance { get; private set; }
+    /// <summary>
+    /// 打开相册中的照片回调
+    /// </summary>
+    public System.Action<string> CallBack_PickImage_With_Base64;
+    /// <summary>
+    /// 保照片到相册的回掉
+    /// </summary>
+    public System.Action<string> CallBack_ImageSavedToAlbum;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(this);
+            return;
+        }
+        Instance = this;
+    }
+
+    /// <summary>
+    /// 保存图片到相册
+    /// </summary>
+    public void SaveToAlbum(string readPath, Texture2D texture)
     {
 #if UNITY_ANDROID
-        StartCoroutine(CutImage("Shot4Share.png"));
+        _saveImageToAndroidAlbum(texture);
 #elif UNITY_IPHONE
-        Application.CaptureScreenshot("Shot4Share.png");
-        string path = Application.persistentDataPath + "/Shot4Share.png";
-        _SavePhoto(path);
+        _iosSaveImageToPhotosAlbum(readPath);
 #endif
     }
 
 #if UNITY_IPHONE
     [DllImport("__Internal")]
-    private static extern void _SavePhoto(string readAddr);
+    private static extern void _iosOpenPhotoLibrary();
+    [DllImport("__Internal")]
+    private static extern void _iosOpenPhotoAlbums();
+    [DllImport("__Internal")]
+    private static extern void _iosOpenCamera();
+    [DllImport("__Internal")]
+    private static extern void _iosOpenPhotoLibrary_allowsEditing();
+    [DllImport("__Internal")]
+    private static extern void _iosOpenPhotoAlbums_allowsEditing();
+    [DllImport("__Internal")]
+    private static extern void _iosOpenCamera_allowsEditing();
+    [DllImport("__Internal")]
+    private static extern void _iosSaveImageToPhotosAlbum(string readAddr);
 
-#endif
-
-#if UNITY_ANDROID
-    private IEnumerator CutImage(string name)
+    /// <summary>
+    /// 打开照片
+    /// </summary>
+    /// <param name="allowsEditing"></param>
+	public static void iosOpenPhotoLibrary(bool allowsEditing = false)
     {
-        //图片大小  
-        Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, true);
-        yield return new WaitForEndOfFrame();
-        tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, true);
-        tex.Apply();
-        yield return tex;
+        if (allowsEditing)
+            _iosOpenPhotoLibrary_allowsEditing();
+        else
+            _iosOpenPhotoLibrary();
+    }
 
-        byte[] byt = tex.EncodeToPNG();
-        string path = "/mnt/sdcard/DCIM/Arphoto/";
+    /// <summary>
+    /// 打开相册
+    /// </summary>
+    /// <param name="allowsEditing"></param>
+	public static void iosOpenPhotoAlbums(bool allowsEditing = false)
+    {
+        if (allowsEditing)
+            _iosOpenPhotoAlbums_allowsEditing();
+        else
+            _iosOpenPhotoAlbums();
+    }
+
+    /// <summary>
+    /// 打开相机
+    /// </summary>
+    /// <param name="allowsEditing"></param>
+	public static void iosOpenCamera(bool allowsEditing = false)
+    {
+        if (allowsEditing)
+            _iosOpenCamera_allowsEditing();
+        else
+            _iosOpenCamera();
+    }
+
+
+    /// <summary>
+    /// 将ios传过的string转成u3d中的texture
+    /// </summary>
+    /// <param name="base64"></param>
+    /// <returns></returns>
+	public static Texture2D Base64StringToTexture2D(string base64)
+    {
+        Texture2D tex = new Texture2D(4, 4, TextureFormat.ARGB32, false);
         try
         {
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-            File.WriteAllBytes(path + name, byt);
+            byte[] bytes = System.Convert.FromBase64String(base64);
+            tex.LoadImage(bytes);
         }
-        catch (Exception e)
+        catch (System.Exception ex)
         {
-            msgText.text = e.Message;
+            Debug.LogError(ex.Message);
         }
+        return tex;
+    }
+
+    /// <summary>
+    /// 打开相册相机后的从ios回调到unity的方法(传图)
+    /// </summary>
+    /// <param name="base64">Base64.</param>
+    void PickImageCallBack_Base64(string base64)
+    {
+        if (CallBack_PickImage_With_Base64 != null)
+        {
+            CallBack_PickImage_With_Base64(base64);
+        }
+    }
+
+    /// <summary>
+    /// 示例
+    /// </summary>
+    /// <param name="base64"></param>
+    void callback_PickImage_With_Base64(string base64)
+    {
+        Texture2D tex = Base64StringToTexture2D(base64);
+        //rawImage.texture = tex;
+    }
+
+    /// <summary>
+    /// 保存图片到相册后，从ios回调到unity的方法
+    /// </summary>
+    /// <param name="msg">Message.</param>
+    void SaveImageToPhotosAlbumCallBack(string msg)
+    {
+        if (CallBack_ImageSavedToAlbum != null)
+        {
+            CallBack_ImageSavedToAlbum(msg);
+        }
+    }
+
+#elif UNITY_ANDROID
+    /// <summary>
+    /// 是否可以尝试直接拷贝
+    /// </summary>
+    /// <param name="texture"></param>
+    private void _saveImageToAndroidAlbum(Texture2D texture)
+    {
+        byte[] byt = texture.EncodeToPNG();
+        string path = "/mnt/sdcard/DCIM/Arphoto/";
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+        File.WriteAllBytes(path + name, byt);
         string[] paths = new string[1];
         paths[0] = path;
         ScanFile(paths);
